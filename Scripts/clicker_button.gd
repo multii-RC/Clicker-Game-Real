4e2d8b340d2cf892f2f3608c3bb08d93545673c1
multii-RC <multilifestyles@gmail.com>
2026-09-09 02:45:53 +0900

    coin appears on start, but not when clicked

--- Scripts/clicker_button.gd
+++ Scripts/clicker_button.gd
@@ -3,6 +3,8 @@ extends Button
 @export var ClickerStrength: int
 signal ClickerSignal(int)
 
+@onready var CoinScene: PackedScene = load("res://Scenes/coin.tscn")
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	ClickerStrength = 10
@@ -20,3 +22,5 @@ func _on_upgrade_button_pressed() -> void:
 
 func on_pressed() -> void:
 	ClickerSignal.emit(ClickerStrength)
+	var c = CoinScene.instantiate()
+	c.global_position = get_global_mouse_position()
