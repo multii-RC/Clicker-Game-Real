7fe82457738c4361387ad9e03917056e9a67e101
multii-RC <multilifestyles@gmail.com>
2026-09-09 03:23:43 +0900

    passive gen amount fix

--- Scripts/GameManager.gd
+++ Scripts/GameManager.gd
@@ -43,7 +43,7 @@ func _on_passive_upgrade_button_pressed() -> void:
 	print("Passive Upgrade Pressed")
 	# Check Cost
 	if coin >= PassiveCost:
-		PassiveStrength = PassiveStrength + 1 * 2
+		PassiveStrength = (PassiveStrength + 1) * 2
 		coin = coin - PassiveCost
 		PassiveCost = PassiveCost * 10
 	# If have money: Double strength, Raise price	
