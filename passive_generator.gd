ddfebca8bfb5407fee08f6f7b7f67562917e9d09
multii-RC <multilifestyles@gmail.com>
2026-09-08 21:08:34 +0900

    Start of Generator

---
+++ passive_generator.gd
@@ -0,0 +1,25 @@
+extends Control
+
+var strength: int
+signal coin_generator(int)
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_upgrade_button_pressed() -> void:
+
+	# Check cost
+	# If haave money
+	# Double Strength, raise cost
+
+func _on_timer_timeout() -> void:
+
+	# Emit Signal
+	# Effects (Coin, Sound)
